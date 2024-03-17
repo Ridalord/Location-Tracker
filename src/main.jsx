19321f237e9ef6aa3411afb192bcd4698e6bd3cb
Ridwan <ridhwan.alabi@gmail.com>
2024-03-17 21:50:15 +0100

    update basename

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='locationTracker'>
+    <BrowserRouter basename='Location-Tracker'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
